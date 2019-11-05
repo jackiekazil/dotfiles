d2bb09a28d279c6f88abfe65b229da81c3a60c2c
jackiekazil <jackiekazil@gmail.com>
2019-11-05 15:41:14 -0500

    Adding a future todo note.

--- setup_scripts/dotfile_link.sh
+++ setup_scripts/dotfile_link.sh
@@ -5,6 +5,8 @@ echo "DOTFILES location..."
 echo $DOTFILES
 echo "This means that this script will link to files in your dotfiles dir there."
 
+# TODO Moving to Oh my zsh. Git consumizations in bash_profile should be removed. 
+
 ln -s $DOTFILES/bashrc ~/.bashrc
 ln -s $DOTFILES/bash_profile ~/.bash_profile
 ln -s $DOTFILES/git-prompt.sh ~/.git-prompt.sh
